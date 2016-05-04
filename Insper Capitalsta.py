c46967fb96d446ff4dc827048161ec9ec3c67266
Franciol <fran.aveiro@hotmail.com>
2016-05-04 10:56:11 -0300

    Nova Versao

--- Insper Capitalsta.py
+++ Insper Capitalsta.py
@@ -11,17 +11,22 @@ from pygame.locals import *
     
    
 # Imagens Importadas
-Insper_background = pygame.image.load('8-Bit-Insper1.png')
+Insper_background = pygame.image.load('8-Bit-Insper1.jpg')
 icone = pygame.image.load('insperLogo.jpg')
 aluno = pygame.image.load('Aluno.png')
 dollar = pygame.image.load('Dollars.png')
 FabLab = pygame.image.load('FabLab 8Bit.png')
+Fabulas = pygame.image.load('Calculo.png')
+
+#Botao Start
 Icone_start = pygame.image.load('New Piskel.png')
 Icone_start2 = pygame.image.load('New Piskel(1).png')
 
 
 #iniciando o pygame
 pygame.init()
+
+green = (0,0,0)
 
 
 #Icone do Jogo
@@ -33,7 +38,7 @@ clock.tick(60)
 
 #Iniciar Display e set de Resolução RESOLUÇAO DEFINIDA EM 768x531 POR CAUSA DAS IMAGENS
 gameDisplay = pygame.display.init()
-screen = pygame.display.set_mode((768,531))
+screen = pygame.display.set_mode((1920,1080))
 pygame.display.toggle_fullscreen()
 
 #Shortcut pra Key
@@ -41,19 +46,24 @@ key = pygame.key
 
 def Mouse():
     X,Y = pygame.mouse.get_pos()
-    pygame.mouse.set_visible(False)
+    pygame.mouse.set_visible(True)
+    return(X,Y)
 
-
-
+def dinheiro(count):
+    font = pygame.font.SysFont(None,100)
+    text = font.render("Dollars:$"+str(count),True,(0,200,0))
+    screen.blit(text,(50,100))
 
 
 #Definir função menu
 def Menu():
     Menu  = False
+    Xmouse,Ymouse = Mouse()
     while not Menu:
-        screen.blit(Insper_background,[0, 0])
-       
-         
+        
+        screen.blit(FabLab,[0, 0])
+        screen.blit(Icone_start2,[400,50])
+        pygame.draw.rect(screen,(0,0,0),(463,338,898,449),1)
     
         for event in pygame.event.get():
                 
@@ -69,31 +79,73 @@ def Menu():
                     
                 if event.type == pygame.QUIT:
                     pygame.quit()
+        
+                 #Botao Start
+                if 1361 > Xmouse > 463:
+                    if 787 > Ymouse > 338:
+                        if pygame.mouse.get_pressed()[0]:
+                            Menu = True
+                        
+                        
+                        
         pygame.display.update()
  
 
 #definir função Jogo       
-def game_loop():            
+def game_loop():     
+
+
+
+       
     #Working Condition
     Crashed = False
-    while not Crashed:
-        screen.blit(FabLab, [0, 0])
+    count = 0
+    Multi = 1
     
+    
+    
+    
+    while not Crashed:
+        screen.blit(Insper_background, [0, 0])
+        screen.blit(aluno,[0,200])
         
+        if count >= 200:
+            screen.blit(Fabulas,[1600,50])
+            pygame.draw.rect(screen,(0,0,0),(1600,50,225,225),1)
+        pygame.draw.rect(screen, (0,0,0),(0,200,400,467),1)
+        Xmouse,Ymouse = Mouse()
+        dinheiro(count)
         for event in pygame.event.get():
-            
+            print(event)
             if key.get_pressed()[K_ESCAPE]:
                 Crashed = True
+                pygame.quit()
             
             if key.get_pressed()[K_F11]:
                 pygame.display.toggle_fullscreen()
                 
             if event.type == QUIT:
                 pygame.quit()
-        pygame.display.update()
+        
+            #Botao Aluno
+            if 0+400 > Xmouse > 0:
+                if 200+460 > Ymouse > 200:
+                    if pygame.mouse.get_pressed()[0]:
+                        count += 1*Multi
+            #Botao Livro 
+            if count >= 200:
+                if 1600+225 > Xmouse > 1600:
+                    if 50+225 > Ymouse > 50:
+                        if pygame.mouse.get_pressed()[0]:
+                            Multi *= 2
+                            count -= 200
+        pygame.display.update()                
+        
+        
+
+                
+                
                 
 #Rodando o Jogo 
-Mouse()
 Menu()
 game_loop() 
-    
