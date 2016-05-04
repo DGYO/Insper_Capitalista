4ef7d54d62773046f6254dab35f69dc9b5d75e47
Franciol <fran.aveiro@hotmail.com>
2016-05-03 22:08:22 -0300

    Definicao de funcoes
    Definicao de funções para facilitar trabalho de alteracao

--- Insper Capitalsta.py
+++ Insper Capitalsta.py
@@ -9,44 +9,91 @@ from pygame.locals import *
 
 
     
-    
+   
 # Imagens Importadas
 Insper_background = pygame.image.load('8-Bit-Insper1.png')
 icone = pygame.image.load('insperLogo.jpg')
 aluno = pygame.image.load('Aluno.png')
 dollar = pygame.image.load('Dollars.png')
 FabLab = pygame.image.load('FabLab 8Bit.png')
+Icone_start = pygame.image.load('New Piskel.png')
+Icone_start2 = pygame.image.load('New Piskel(1).png')
 
-#Icone do Jogo
-pygame.display.set_icon(icone)
 
 #iniciando o pygame
 pygame.init()
 
 
+#Icone do Jogo
+pygame.display.set_icon(icone)
 
+#Fixando FPS
 clock = pygame.time.Clock()
+clock.tick(60)
 
 #Iniciar Display e set de Resolução RESOLUÇAO DEFINIDA EM 768x531 POR CAUSA DAS IMAGENS
 gameDisplay = pygame.display.init()
 screen = pygame.display.set_mode((768,531))
+pygame.display.toggle_fullscreen()
 
 #Shortcut pra Key
 key = pygame.key
 
-#Working Condition
-Crashed = False
+def Mouse():
+    X,Y = pygame.mouse.get_pos()
+    pygame.mouse.set_visible(False)
 
 
 
-while not Crashed:
-    screen.blit(Insper_background, [0, 0])
 
+
+#Definir função menu
+def Menu():
+    Menu  = False
+    while not Menu:
+        screen.blit(Insper_background,[0, 0])
+       
+         
     
-    for event in pygame.event.get():
+        for event in pygame.event.get():
+                
+                if key.get_pressed()[K_ESCAPE]:
+                    Menu = True
+                    pygame.quit()
+                    
+                if key.get_pressed()[K_F11]:
+                    pygame.display.toggle_fullscreen()
+                    
+                if key.get_pressed()[K_1]:
+                    Menu = True
+                    
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+        pygame.display.update()
+ 
+
+#definir função Jogo       
+def game_loop():            
+    #Working Condition
+    Crashed = False
+    while not Crashed:
+        screen.blit(FabLab, [0, 0])
+    
         
-        if key.get_pressed()[K_ESCAPE]:
-            Crashed = True
-        
-        if key.get_pressed()[K_F11]:
-            pygame.display.toggle_fullscreen()
+        for event in pygame.event.get():
+            
+            if key.get_pressed()[K_ESCAPE]:
+                Crashed = True
+            
+            if key.get_pressed()[K_F11]:
+                pygame.display.toggle_fullscreen()
+                
+            if event.type == QUIT:
+                pygame.quit()
+        pygame.display.update()
+                
+#Rodando o Jogo 
+Mouse()
+Menu()
+game_loop() 
+    
