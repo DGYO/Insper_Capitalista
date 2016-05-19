0ae192663a1049972c7c9eb4202b70dd3a35c7fd
franciol <fran.aveiro@hotmail.com>
2016-05-19 08:10:19 -0300

    Ajuste dos botoes

--- Insper Capitalsta.py
+++ Insper Capitalsta.py
@@ -15,7 +15,7 @@ alunoR = pygame.image.load('alunocapitalistasR.png')
 dollar = pygame.image.load('Dollars.png')
 FabLab = pygame.image.load('FabLab 8Bit.png')
 Fabulas = pygame.image.load('Calculo.png')
-calculosemdinheiro = pygame.image.load('calculosemdinheiro.png')
+calculosemdinheiro = pygame.image.load('Calculosemdinheiro.png')
 FabulasBW = pygame.image.load('CalculoBW.png')
 Cafe = pygame.image.load("coffe.png")
 cafesemdinheiro = pygame.image.load("cafesemdinheiro.png")
@@ -63,15 +63,20 @@ def dinheiro(count):
 def contagem_livros(livros):
     font = pygame.font.SysFont("heineken",50)
     text = font.render("livros:"+str(livros),True,(0,200,0))
-    screen.blit(text,(850,30))
+    screen.blit(text,(850,20))
 
 def contagem_cafes(cafes):
     font = pygame.font.SysFont("    heineken",50)
     text = font.render("Cafes:"+str(cafes),True,(0,200,0))
-    screen.blit(text,(850,70))
+    screen.blit(text,(850,90))
+
+def contagem_acai(acais):
+    font = pygame.font.SysFont("    heineken",50)
+    text = font.render("Acais:"+str(acais),True,(0,200,0))
+    screen.blit(text,(850,165))
  
-def Save(count,inflacaocafe,inflacaolivro,Multi,livros,MultiT1,cafes):
-    listData = [count,inflacaocafe,inflacaolivro,Multi,livros,MultiT1,cafes]
+def Save(count,Multi,MultiT1,inflacaolivro,inflacaocafe,inflacaoacai,inflacaocomp,inflacaoninja,inflacaocarro,inflacaoprof,inflacaoent,inflacaoFab,inflacaoInsper,livros,cafes,acais,comps,ninjas,carros,profs,entidades,FabLabs,Inspers):
+    listData = [count,Multi,MultiT1,inflacaolivro,inflacaocafe,inflacaoacai,inflacaocomp,inflacaoninja,inflacaocarro,inflacaoprof,inflacaoent,inflacaoFab,inflacaoInsper,livros,cafes,acais,comps,ninjas,carros,profs,entidades,FabLabs,Inspers]
     outFile = open('DataSave.txt','wb')
     pickle.dump(listData, outFile)
     outFile.close()
@@ -85,13 +90,28 @@ def Load():
 def Reset():
     count = 0
     inflacaolivro = 1.0
-    inflacaocafe = 1.0  
+    inflacaocafe = 1.0   
+    inflacaoacai = 1
+    inflacaocomp = 1
+    inflacaoninja = 1
+    inflacaocarro = 1
+    inflacaoprof = 1
+    inflacaoent = 1
+    inflacaoFab = 1
+    inflacaoInsper = 1
     Multi = 1
     MultiT1 = 1
-    cafes = 0
     livros = 0
-    return count,inflacaolivro,inflacaocafe,Multi,MultiT1,cafes,livros
-    
+    cafes = 0
+    acais = 0
+    comps = 0
+    ninjas = 0
+    carros = 0
+    profs = 0
+    entidades = 0
+    FabLabs = 0
+    Inspers = 0
+    return count,Multi,MultiT1,inflacaolivro,inflacaocafe,inflacaoacai,inflacaocomp,inflacaoninja,inflacaocarro,inflacaoprof,inflacaoent,inflacaoFab,inflacaoInsper,livros,cafes,acais,comps,ninjas,carros,profs,entidades,FabLabs,Inspers  
     
     
     
@@ -103,59 +123,115 @@ def Reset():
 
 #definir função Jogo            
 
+
 #variáveis
-inflacaolivro = 1.0
-inflacaocafe = 1.0   
 DINHEIROTEMPO = USEREVENT + 1 
 pygame.time.set_timer(DINHEIROTEMPO, 1000)
+
 #Working Condition
 Crashed = False
 count = 0 
 Multi = 1
-livros = 0
-cafes = 0
 MultiT1 = 1
 
+#inflacao
+inflacaolivro = 1.0
+inflacaocafe = 1.0   
+inflacaoacai = 1
+inflacaocomp = 1
+inflacaoninja = 1
+inflacaocarro = 1
+inflacaoprof = 1
+inflacaoent = 1
+inflacaoFab = 1
+inflacaoInsper = 1
+
+#Quantidade
+livros = 0
+cafes = 0
+acais = 0
+comps = 0
+ninjas = 0
+carros = 0
+profs = 0
+entidades = 0
+FabLabs = 0
+Inspers = 0
     
     
 while not Crashed:
     screen.blit(Insper_background, [0, 0])
     screen.blit(aluno,[-40,197])
-    
-    screen.blit(imagembotao,[600,150])
-    screen.blit(imagembotao,[600,225])
-    screen.blit(imagembotao,[600,300])
-    screen.blit(imagembotao,[600,375])
-    screen.blit(imagembotao,[600,450])
-    screen.blit(imagembotao,[600,525])
-    screen.blit(imagembotao,[600,600])
-    screen.blit(imagembotao,[600,675])
-
-
-    #aluno e livro
+      
+    #livro
     if count >= 200 * inflacaolivro:
-        screen.blit(imagembotao,[600,0])
-        screen.blit(Fabulas,[610,10])
+        livrobut = screen.blit(imagembotao,[600,0])
+        screen.blit(Fabulas,[620,15])
     elif count < 200 * inflacaolivro:
-        screen.blit(botaosemdinheiro,[600,0])
-        screen.blit(calculosemdinheiro,[610,10])
-        
-    dinheiro(count)
-    contagem_livros(livros)
-
+        livrobut = screen.blit(botaosemdinheiro,[600,0])
+        screen.blit(calculosemdinheiro,[620,15])
     #cafe
     if count >= 300 * inflacaocafe:
-        screen.blit(imagembotao,[600,75])
-        screen.blit(Cafe,[610,85])
-
+        cafebut = screen.blit(imagembotao,[600,75]) 
+        screen.blit(Cafe,[620,80])  
     elif count < 300 * inflacaocafe:
-        screen.blit(botaosemdinheiro,[600,75])
-        screen.blit(cafesemdinheiro,[610,85])
-        
+        cafebut = screen.blit(botaosemdinheiro,[600,75])
+        screen.blit(cafesemdinheiro,[620,80])
+    #acai
+    if count >= 300 * inflacaoacai:
+        acaibut = screen.blit(imagembotao,[600,150])   
+    elif count < 300 * inflacaoacai:
+        acaibut = screen.blit(botaosemdinheiro,[600,150])
+    
+    #computador
+    if count >= 300 * inflacaocomp:
+        compbut = screen.blit(imagembotao,[600,225])   
+    elif count < 300 * inflacaocomp:
+        compbut = screen.blit(botaosemdinheiro,[600,225])
+    
+    #Ninja
+    if count >= 300 * inflacaoninja:
+        ninjabut = screen.blit(imagembotao,[600,300])
+    elif count < 300 * inflacaoninja:
+        ninjabut = screen.blit(botaosemdinheiro,[600,300])
+    
+    #Carro
+    if count >= 300 * inflacaocarro:
+        carrobut = screen.blit(imagembotao,[600,375])
+    elif count < 300 * inflacaocarro:
+        carrobut = screen.blit(botaosemdinheiro,[600,375])
+
+    #Prof
+    if count >= 300 * inflacaoprof:
+        profbut = screen.blit(imagembotao,[600,450])
+    elif count < 300 * inflacaoprof:
+        profbut = screen.blit(botaosemdinheiro,[600,450])
+    
+    #entidade
+    if count >= 300 * inflacaoent:
+        entidadebut = screen.blit(imagembotao,[600,525])
+    elif count < 300 * inflacaoent:
+        entidadebut = screen.blit(botaosemdinheiro,[600,525])
+    
+    #FabLab
+    if count >= 300 * inflacaoFab:
+        fablabbut =  screen.blit(imagembotao,[600,600])
+    elif count < 300 * inflacaoFab:
+        fablabbut = screen.blit(botaosemdinheiro,[600,600])
+    
+    #Insper
+    if count >= 300 * inflacaoInsper:
+        insperbut = screen.blit(imagembotao,[600,675])
+    elif count < 300 * inflacaoInsper:
+        insperbut = screen.blit(botaosemdinheiro,[600,675])
+   
+    
        
     Xmouse,Ymouse = Mouse()
     dinheiro(count)
     contagem_cafes(cafes)
+    contagem_livros(livros)
+    contagem_acai(acais)
     
     
     for event in pygame.event.get():
@@ -169,13 +245,13 @@ while not Crashed:
                 pygame.display.toggle_fullscreen()
 
             elif event.key == K_s:
-                Save(count,inflacaocafe,inflacaolivro,Multi,livros,MultiT1,cafes)  
+                Save(count,Multi,MultiT1,inflacaolivro,inflacaocafe,inflacaoacai,inflacaocomp,inflacaoninja,inflacaocarro,inflacaoprof,inflacaoent,inflacaoFab,inflacaoInsper,livros,cafes,acais,comps,ninjas,carros,profs,entidades,FabLabs,Inspers)  
                 
             elif event.key == K_l:
-                count,inflacaocafe,inflacaolivro,Multi,livros,MultiT1,cafes = Load()
+                count,Multi,MultiT1,inflacaolivro,inflacaocafe,inflacaoacai,inflacaocomp,inflacaoninja,inflacaocarro,inflacaoprof,inflacaoent,inflacaoFab,inflacaoInsper,livros,cafes,acais,comps,ninjas,carros,profs,entidades,FabLabs,Inspers = Load()
                 
             elif event.key == K_r:
-                count,inflacaocafe,inflacaolivro,Multi,MultiT1,cafes,livros = Reset()
+                count,Multi,MultiT1,inflacaolivro,inflacaocafe,inflacaoacai,inflacaocomp,inflacaoninja,inflacaocarro,inflacaoprof,inflacaoent,inflacaoFab,inflacaoInsper,livros,cafes,acais,comps,ninjas,carros,profs,entidades,FabLabs,Inspers = Reset()
                 
         elif event.type == QUIT:
             pygame.quit()
@@ -191,10 +267,58 @@ while not Crashed:
                 if 197+380 > Ymouse > 197:
                     aluno =  alunoR
                     screen.blit(aluno,[-40,197])
+            
             if 600+500 > Xmouse > 600:
                 if 0+75 > Ymouse > 0:
-                    imagembotao = imagembotaoapertado
-                    screen.blit(imagembotao,[600,0])
+                	if count >= 200 * inflacaolivro:
+	                    livrobut = imagembotaoapertado
+	                    screen.blit(livrobut,[600,0])
+
+                if 150 > Ymouse > 75:
+                    if count >= 300 * inflacaocafe:
+	                    cafebut = imagembotaoapertado
+	                    screen.blit(cafebut,[600,75])
+
+                if 225 > Ymouse > 150:
+                	if count >= 300 * inflacaoacai:
+	                    acaibut = imagembotaoapertado
+	                    screen.blit(acaibut,[600,150])
+
+                if 300 > Ymouse > 225:
+                	if count >= 300 * inflacaocomp:
+	                    compbut = imagembotaoapertado
+	                    screen.blit(compbut,[600,225])
+                
+                if 375 > Ymouse > 300:
+                    if count >= 300 * inflacaoninja:
+	                    ninjabut = imagembotaoapertado
+	                    screen.blit(ninjabut,[600,300])
+
+                if 450 > Ymouse > 375:
+                	if count >= inflacaocarro:
+	                    carrobut = imagembotaoapertado
+	                    screen.blit(carrobut,[600,375])
+
+                if 525 > Ymouse > 450:
+                	if count >= 300 * inflacaoprof:
+	                    profbut = imagembotaoapertado
+	                    screen.blit(profbut,[600,450])
+
+                if 600 > Ymouse > 525:
+                	if count >= 300 * inflacaoent:
+	                    entidadebut = imagembotaoapertado
+	                    screen.blit(entidadebut,[600,525])
+	                
+                if 675 > Ymouse > 600:
+                    if count >= 300 * inflacaoFab:
+	                    fablabbut = imagembotaoapertado
+	                    screen.blit(fablabbut,[600,600])
+                
+                if 825 > Ymouse > 675:
+                	if count >= 300 * inflacaoInsper:
+	                    insperbut = imagembotaoapertado
+	                    screen.blit(insperbut,[600,675])
+
 
         elif event.type == MOUSEBUTTONUP:
             Xmouse,Ymouse = event.pos
@@ -204,12 +328,63 @@ while not Crashed:
                     screen.blit(aluno,[-40,197])
             if 600+500 > Xmouse > 600:
                 if 0+75 > Ymouse > 0:
-                    imagembotao = pygame.image.load("botao.png")   
-                    screen.blit(imagembotao,[600,0]) 
+                	if count >= 200 * inflacaolivro:
+	                    livrobut = imagembotao
+	                    screen.blit(livrobut,[600,0])
+	                    livros += 1
+	                    Multi += 2
+	                    count -= 200 * inflacaolivro
+	                    inflacaolivro *= 1.5
 
+                if 0+150 > Ymouse > 75:
+	                if count >= 300 * inflacaocafe:
+	                    cafebut = imagembotao
+	                    screen.blit(cafebut,[600,75])
+	                    cafes += 1
+	                    MultiT1 += 2
+	                    count -= 300 * inflacaocafe
+	                    inflacaocafe *= 1.7  
 
+                if 0+225 > Ymouse > 150:
+                	if count >= 300 * inflacaoacai:
+	                    acaibut = imagembotao
+	                    screen.blit(acaibut,[600,150])
 
-        
+                if 0+300 > Ymouse > 225:
+                	if count >= 300 * inflacaocomp:
+	                    compbut = imagembotao
+	                    screen.blit(compbut,[600,225])
+                
+                if 0+375 > Ymouse > 300:
+                	if count >= 300 * inflacaoninja:
+	                    ninjabut = imagembotao
+	                    screen.blit(ninjabut,[600,300])
+
+                if 0+450 > Ymouse > 375:
+                	if count >= 300 * inflacaocarro:
+	                    carrobut = imagembotao
+	                    screen.blit(carrobut,[600,375])
+
+                if 0+525 > Ymouse > 450:
+                	if count >= 300 * inflacaoprof:
+	                    profbut = imagembotao
+	                    screen.blit(profbut,[600,450])
+
+                if 0+600 > Ymouse > 525:
+                	if count >= 300 * inflacaoent:
+	                    entidadebut = imagembotao
+	                    screen.blit(entidadebut,[600,525])
+                
+                if 0+675 > Ymouse > 600:
+                	if count >= 300 * inflacaoFab:
+	                    fablabbut = imagembotao
+	                    screen.blit(fablabbut,[600,600])
+                
+                if 0+825 > Ymouse > 675:
+                	if count >= 300 * inflacaoInsper:
+	                    insperbut = imagembotao
+	                    screen.blit(insperbut,[600,675])
+
             
 
             #Botao Aluno
@@ -217,23 +392,7 @@ while not Crashed:
                 if 197+380 > Ymouse > 197:       
                     count += 1*Multi
                     
-            #Botao Livro 
-            if count >= 200 * inflacaolivro:
-                if 600+500 > Xmouse > 600:
-                    if 0+75 > Ymouse > 0:
-                            livros += 1
-                            Multi *= 2
-                            count -= 200
-                            inflacaolivro *= 1.5
-                            
-            #Botao Cafe
-            if count >= 300 * inflacaocafe:
-                if 600+225 > Xmouse > 600:
-                    if 300+225 > Ymouse > 300:
-                        cafes += 1
-                        MultiT1 *= 2
-                        count -= 300
-                        inflacaocafe *= 1.7  
+              
                         
 
 
